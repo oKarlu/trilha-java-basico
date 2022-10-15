6d655b675143bfec7c7e87f63af58bda0c14b35b
Carlos Marinho <cadumsantos@hotmail.com>
2022-10-15 19:04:13 -0300

    Tipo primitivo, constante, string, char, boolean

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,28 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Tipos primitivos
+        byte idade = 123;
+        short ano = 2021;
+        int cep= 72999999;
+        long cpf = 11111111111L;
+        float PI = 3.14F;
+        double f = 1275.33;
+
+        //Constante
+        final double VALOR_DE_PI = 3.14;
+
+        //String para receber texto
+        String nome = "Carlos";
+        String sobrenome = "Marinho";
+        String nomeComleto = nome.concat(" ").concat(sobrenome);
+
+        //Char para receber caractere
+        char caractere1 = 'a';
+        char caractere2 = 'b';
+        char caractere3 = 'c';
+
+        //Lógico
+        boolean maiorDeIdade = true;
+
     }
 }
